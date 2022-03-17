1db431f3d9729f65e3ac9f56fb6dc993bf0801bc
Ali yoho <studycatcat@yahoo.com>
2022-03-17 10:58:34 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,38 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+-- CREATE TABLE messages (
+--   /* Describe your table here.*/
+--   'id' ,
+--   message varchar(255),
+-- );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE `Users` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `username` MEDIUMTEXT NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
+CREATE TABLE `Messages` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `Message` MEDIUMTEXT NULL DEFAULT NULL,
+  `Timestamp` TIME NULL DEFAULT NULL,
+  `Room Name` MEDIUMTEXT NULL DEFAULT NULL,
+  `id_Users` INTEGER NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `Messages` ADD FOREIGN KEY (id_Users) REFERENCES `Users` (`id`);
+
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- INSERT INTO `Users` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `Messages` (`id`,`Message`,`Timestamp`,`Room Name`,`id_Users`) VALUES
+-- ('','','','','');
 
 
 /*  Execute this file from the command line by typing:
